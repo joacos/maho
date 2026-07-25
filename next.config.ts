7f23322f326e7b5584d934d961a99787104702cc
joacos <joaquinj19@gmail.com>
2026-07-24 23:26:14 -0400

    chore: configure project for Vercel migration

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   trailingSlash: true,
   allowedDevOrigins: ['192.168.1.12'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
